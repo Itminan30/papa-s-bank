fd24edab1997a2332b5a28611cbbccc07746ed7b
Itminan30 <itminan30@gmail.com>
2023-02-14 19:18:21 +0600

    another commit

--- script/deposit.js
+++ script/deposit.js
@@ -6,7 +6,7 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     // step-3: get the current deposit total
     const depositTotalElement = document.getElementById('deposit-total');
     const previousDepositTotal = depositTotalElement.innerText;
-    const currentDepositTotal = parseFloat(previousDepositTotal)+parseFloat(newdDepositAmount);
+    const currentDepositTotal = parseFloat(previousDepositTotal)+parseFloats(newdDepositAmount);
     depositTotalElement.innerText = currentDepositTotal;
 
     // step-7: clear deposit field
