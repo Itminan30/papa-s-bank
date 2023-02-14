33e184507ba72fec5058a72bc578c3f19d064a07
Itminan30 <itminan30@gmail.com>
2023-02-14 19:37:15 +0600

    updated balance counter

--- script/deposit.js
+++ script/deposit.js
@@ -6,9 +6,15 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     // step-3: get the current deposit total
     const depositTotalElement = document.getElementById('deposit-total');
     const previousDepositTotal = depositTotalElement.innerText;
-    const currentDepositTotal = parseFloat(previousDepositTotal)+parseFloats(newdDepositAmount);
+    const currentDepositTotal = parseFloat(previousDepositTotal)+parseFloat(newdDepositAmount);
+    // step-4: add numbers to set the total deposit
     depositTotalElement.innerText = currentDepositTotal;
-
+    // step-5: get previous balance total
+    const previousBalanceElement = document.getElementById('balance-total');
+    const previousBalanceTotal =  previousBalanceElement.innerText;
+    const newBalanceTotal = newdDepositAmount;
+    // step-6: update previous balance total
+    previousBalanceElement.innerText = parseFloat(newBalanceTotal) + parseFloat(previousBalanceTotal) ;
     // step-7: clear deposit field
     depositField.value = '';
 })
