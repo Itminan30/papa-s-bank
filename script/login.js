8fdf77ec20a7891d8df0347599da1f7902dd9531
Itminan30 <itminan30@gmail.com>
2023-02-14 17:23:13 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,18 @@
+// step-1: add click event handler
+document.getElementById('submit-btn').addEventListener('click', function(event){
+    // step-2: get email from user and use value to get the input field's value
+    const userMail = document.getElementById('user-mail').value;
+    // step-3: get password similarly to the email
+    const userPassoword = document.getElementById('user-password').value;
+    // Dangerous: Don't verify mail and password on the client side
+    // step-4: verify mail and password
+
+    if(userMail === 'goribPola@takaNai.com' && userPassoword === 'takadao'){
+        console.log('Ashol pola');
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert('er baap ami na');
+    }
+
+})
