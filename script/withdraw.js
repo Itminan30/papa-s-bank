0dfa5e3d2419d610d435d51f34dc536b89b9c947
Itminan30 <itminan30@gmail.com>
2023-02-14 20:59:10 +0600

    project finished

--- script/withdraw.js
+++ script/withdraw.js
@@ -1,4 +1,35 @@
 // step-1: add event handler in withdraw-btn
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    const withrawFieldElement = document.getElementById('withdraw-field');
+    // step-2:
+    const withdrawFieldElement = document.getElementById('withdraw-field');
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmount = parseFloat(withdrawField.value);
+    // step-3:
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
+    
+    // step-5:
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText) ;
+
+    // step-7:
+    withdrawFieldElement.value = '';
+    
+    if(newWithdrawAmount>previousBalanceTotal){
+        alert("Baap er bank e eto taka nai");
+        return;
+    }
+
+    // step-4:
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step-6:
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    // step-7:
+    withdrawFieldElement.value = '';
+
+
 })
