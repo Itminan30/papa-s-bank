013556db25c52033c5b648e5932d567f459b88e4
Itminan30 <itminan30@gmail.com>
2023-02-14 20:00:43 +0600

    step-1 completed

--- script/withdraw.js
+++ script/withdraw.js
@@ -1,4 +1,4 @@
 // step-1: add event handler in withdraw-btn
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    
+    const withrawFieldElement = document.getElementById('withdraw-field');
 })
